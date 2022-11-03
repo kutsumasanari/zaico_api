b907ab2d8e3716fe3d09fa69e59a33f05865d5c7
kutsumasanari <108452474+kutsumasanari@users.noreply.github.com>
2022-11-03 23:26:00 +0900

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,7 @@
-python3 -m pip install --upgrade pip
+sudo apt update
+sudo apt upgrade
+sudo apt install python3-pip
+sudo python3 get-pip.py
 pip install virtualenv
 cd /home/ec2-user/zaico_api
 virtualenv environment

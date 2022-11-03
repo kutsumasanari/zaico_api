a06e35a84d034844554a9c17e251bb3c4517bc46
kutsumasanari <108452474+kutsumasanari@users.noreply.github.com>
2022-11-03 23:39:54 +0900

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,10 +2,10 @@ sudo apt update
 sudo apt upgrade
 sudo apt install python3-pip
 sudo python3 get-pip.py
-pip install virtualenv
+pip3 install virtualenv
 cd /home/ec2-user/zaico_api
 virtualenv environment
 source environment/bin/activate
 sudo apt install uvicorn
-pip install fastapi
-pip install sqlalchemy
+pip3 install fastapi
+pip3 install sqlalchemy
